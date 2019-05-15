bb71d71cb09c211092ae3ab4295f21b404eb5d65
migrulos <migrulos@gmail.com>
2019-05-14 22:47:47 -0700

    add shell-scripts to create gcp instance with startup-script

---
+++ gcp-startup-shell-script/gcp-create-instance.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu
+
+gcloud compute instances create apache-test \
+--boot-disk-size=10GB \
+--image-family ubuntu-1604-lts \
+--image-project=ubuntu-os-cloud \
+--machine-type=g1-small \
+--tags http-server \
+--restart-on-failure \
+--zone=europe-west3-a \
+--metadata-from-file startup-script=install-apache2.sh
---
+++ gcp-startup-shell-script/install-apache2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eu
+
+apt-get update
+apt-get install -y apache2
+cat <<EOF > /var/www/html/index.html
+<html><body><h1>Hello World</h1>
+<p>This page was created from a simple startup script!</p>
+</body></html>
+EOF
