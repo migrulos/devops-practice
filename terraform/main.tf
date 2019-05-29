be6d1ddcb1966925fde02c467683deaf7026e531
migrulos <migrulos@gmail.com>
2019-05-29 00:51:05 -0700

    some simple changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,47 @@
 provider "google" {
-    version = "2.7"
-    project = "${var.project}"
-    region = "europe-west1"
+  version = "2.7"
+  project = "${var.project}"
+  region  = "europe-west1"
+}
+
+resource "google_compute_project_metadata" "ssh-keys" {
+    metadata = {
+        ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+    }
 }
 
 resource "google_compute_instance" "example-instance" {
-    name    = "example-name"
-    machine_type = "f1-micro"
-    zone = "europe-west1-b"
-    boot_disk {
-        initialize_params {
-            image = "centos-7"
-        }
+  name         = "example-name"
+  machine_type = "f1-micro"
+  zone         = "europe-west1-b"
+  boot_disk {
+    initialize_params {
+      image = "centos-7"
     }
-#    tags = ["open-5000tcp"]
-    tags = ["http-server"]
-    metadata = {
-        sshKeys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
-    }
+  }
+  #    tags = ["open-5000tcp"]
+  tags = ["http-server"]
 
-    network_interface {
-        network = "default"
-        access_config {}
+  ### changed to project_metadata with "ssh-keys" instead of "sshKeys"
+  #metadata = {
+  #  sshKeys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  #}
+
+  network_interface {
+    network = "default"
+    access_config {
+        # Ephemeral ip
     }
+  }
+
+  metadata_startup_script = "${file("scripts/install-apache.sh")}"
+
+  #    provisioner "local-exec" {
+  #        inline = [
+  #            "sudo /tmp/install-apache.sh"
+  #        ]
+  #    }
+
 }
 
 #resource "google_compute_firewall" "open-port-example" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable project {
-    description = "Project ID"
+  description = "Project ID"
 }
 
 variable gce_ssh_user {
-    description = "SSH-user"
+  description = "SSH-user"
 }
 
 variable gce_ssh_pub_key_file {
-    description = "path to pub_key file"
+  description = "path to pub_key file"
 }
