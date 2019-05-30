23258faa6026ebc71e1b6ee6f69af697e96b7c1c
migrulos <migrulos@gmail.com>
2019-05-30 03:46:50 -0700

    some changes

--- packer/scripts/install-apache.sh
+++ packer/scripts/install-apache.sh
@@ -5,6 +5,7 @@ set -eu
 yum -y update
 yum -y install httpd
 systemctl enable httpd
+systemctl start httpd
 cat <<EOF > /var/www/html/index.html
 <html><body><h1>Hello World</h1>
 <p>This page was created from a simple startup script!</p>
